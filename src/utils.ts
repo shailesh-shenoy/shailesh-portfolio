81a3de0d40984e258824c3afd88782a1d4496773
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2024-05-24 22:01:40 -0400

    Struggles...

--- src/utils.ts
+++ src/utils.ts
@@ -11,6 +11,19 @@ interface StoryItem {
     image: string;
 }
 
+export interface Skill {
+    name: string;
+    iconClass: string;
+}
+
+export interface ProjectData {
+    name: string;
+    description: string;
+    imageUrl: string;
+    github: string;
+    skills: string[];
+}
+
 export const mailToText = `mailto:shenoy.shailesh21@gmail.com?subject=Reaching%20out%20from%20your%20portfolio`;
 
 export const socials: Array<NavItem> = [
@@ -60,6 +73,85 @@ export const stories: Array<StoryItem> = [
         image: "/images/now.jpg",
     },
 ];
+
+export const allSkills: Record<string, Skill> = {
+    "Solidity": {
+        name: "Solidity",
+        iconClass: "devicon-solidity-plain",
+    },
+    "React": {
+        name: 'React',
+        iconClass: 'devicon-react-original',
+    },
+    // Add more skills here
+    "TypeScript": {
+        name: 'TypeScript',
+        iconClass: 'devicon-typescript-plain',
+    },
+    "Python": {
+        name: 'Python',
+        iconClass: 'devicon-python-plain',
+    },
+    "MongoDB": {
+        name: 'MongoDB',
+        iconClass: 'devicon-mongodb-plain',
+    },
+    "C#": {
+        name: 'C#',
+        iconClass: 'devicon-csharp-plain',
+    },
+    "Go": {
+        name: 'Go',
+        iconClass: 'devicon-go-plain',
+    },
+    "AWS": {
+        name: 'AWS',
+        iconClass: 'devicon-amazonwebservices-plain',
+    },
+    "NextJS": {
+        name: 'NextJS',
+        iconClass: 'devicon-nextjs-plain',
+    },
+    "Postgres": {
+        name: 'Postgres',
+        iconClass: 'devicon-postgresql-plain',
+    },
+    "Unity": {
+        name: 'Unity',
+        iconClass: 'devicon-unity-plain',
+    },
+    "Java": {
+        name: 'Java',
+        iconClass: 'devicon-java-plain',
+    },
+};
+
+export const projects: Record<string, ProjectData> = {
+    'VeriFree': {
+        name: 'VeriFree',
+        description: `A permissioned blockchain where students with a valid educational email address can gain access to a student-led DAO as well as free transactions on the verifree Subnet.
+        Runner up prize at Chainlink Constellation hackathon in Avalanche track.`,
+        imageUrl: '/projects/verifree.png',
+        github: 'https://github.com/shailesh-shenoy/VeriFree',
+        skills: ['Solidity', 'NextJS', 'MongoDB', 'TypeScript']
+    },
+    'WagerWinz': {
+        name: 'WagerWinz',
+        description: 'A decentralized, P2P platform for creating, challenging and winning wagers on the price of ETH/USD using smart contracts and Chainlink price feed.',
+        imageUrl: '/projects/wagerwinz.jpg',
+        github: 'https://github.com/shailesh-shenoy/wagerwinz',
+        skills: ['Solidity', 'React', 'Chainlink']
+    },
+    'AllCoinz': {
+        name: "AllCoinz",
+        description: "A simple and easy-to-use crypto portfolio aggregator which let's you track all your coins across top exchanges and blockchains. View your portfolio on the web app or integrate into your own tools using the REST API. Work in Progress.",
+        imageUrl: '/projects/allcoinz.png',
+        github: "",
+        skills: ['Go', "React", "Postgres", "AWS"],
+    },
+};
+
+
 
 
 
